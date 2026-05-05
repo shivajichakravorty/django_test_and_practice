54a6c36b908ac3950f2722bea120d82eb2735425
Shivaji <shivaji.chakravorty5@gmail.com>
2026-05-05 23:18:10 +0530

    Pipelines and different URLs for the shop app

--- practice_mycart_ecom/mycart/shop/urls.py
+++ practice_mycart_ecom/mycart/shop/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="About"),
+    path("contact/", views.contact, name="Contact"),
+    path("tracker/", views.tracker, name="Tracker"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.productview, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
--- practice_mycart_ecom/mycart/shop/views.py
+++ practice_mycart_ecom/mycart/shop/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request, "shop/index.html")
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+def tracker(request):
+    return HttpResponse("Tracker Page")
+
+def search(request):
+    return HttpResponse("Search Page")
+
+def productview(request):
+    return HttpResponse("Product View Page")
+
+def checkout(request):
+    return HttpResponse("Checkout Page")   
