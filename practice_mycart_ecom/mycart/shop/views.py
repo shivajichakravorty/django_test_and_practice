75b86753032453b0baaf9435f15fd0f4d4a1f6e9
Shivaji <shivaji.chakravorty5@gmail.com>
2026-05-13 10:20:22 +0530

    Quick View and rendered other pages - basic

--- practice_mycart_ecom/mycart/shop/views.py
+++ practice_mycart_ecom/mycart/shop/views.py
@@ -1,36 +1,21 @@
-from django.shortcuts import render
-from math import ceil
+from django.shortcuts import get_object_or_404, render
 from .models import Product
+from collections import defaultdict
 
 
 def index(request):
     # Fetch all products from the database
     all_products = Product.objects.all()
-    n = len(all_products)
-    if n == 0:
-        return render(request, 'shop/index.html', {'all_carousels': []})
 
-    # Define how many products per slide (matches col-md-4 for 3 columns)
-    items_per_slide = 3
-    nSlides = ceil(n / items_per_slide)
-    category_set = set(product.category for product in all_products)
-    subcategory_set = set(product.subcategory for product in all_products)
+    if not all_products:
+        return render(request, 'shop/index.html', {'products_by_category': {}})
 
+    # Group products by category
+    products_by_category = defaultdict(list)
+    for product in all_products:
+        products_by_category[product.category].append(product)
 
-    # Group all products into slides
-    
-    all_carousels = []
-
-    # Prepare the data for two carousels, each containing all products
-    for category in category_set:
-        category_products = [product for product in all_products if product.category == category]
-        n_category = len(category_products)
-        nSlides_category = ceil(n_category / items_per_slide)
-        category_slides = [category_products[i:i + items_per_slide] for i in range(0, n_category, items_per_slide)]
-        all_carousels.append({'title': category, 'slides': category_slides, 'range': range(nSlides_category)})
-    
-
-    context = {'all_carousels': all_carousels}
+    context = {'products_by_category': dict(products_by_category)}
     return render(request, 'shop/index.html', context)
 
 def about(request):
@@ -45,8 +30,9 @@ def tracker(request):
 def search(request):
     return render(request, 'shop/search.html')
 
-def productview(request):
-    return render(request, 'shop/productview.html')
+def productview(request, myid):
+    product = get_object_or_404(Product, id=myid)
+    return render(request, 'shop/productview.html', {'product': product})
 
 def checkout(request):
     return render(request, 'shop/checkout.html')
